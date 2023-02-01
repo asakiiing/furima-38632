c60caed13cabde16c3e058a33915301117ccb446
asakiiing <asaki2726@gmail.com>
2023-02-01 17:14:03 +0900

    ActiveHash　カテゴリー選択

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,20 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+
+
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :shipping_cost_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
+  validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, presence: true
   validates :shipping_date_id, presence: true
   validates :price, presence: true, format: {with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }

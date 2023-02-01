7a6f1e29fa836c6964c88eb197b259565ad1c4f0
asakiiing <asaki2726@gmail.com>
2023-02-01 18:34:44 +0900

    ActiveHashクラス名　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,8 +17,8 @@ class Item < ApplicationRecord
   validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :status_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true, format: {with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image, presence: true
 end

0f0bbf8f2963a6c83fe9f877d0523854ac224243
asakiiing <asaki2726@gmail.com>
2023-02-03 12:22:33 +0900

    テストコードと手数料、利益表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,6 +19,6 @@ class Item < ApplicationRecord
   validates :shipping_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, presence: true, format: {with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, format: {with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be 300~9999999" }
   validates :image, presence: true
 end
